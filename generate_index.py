15c9e1ff5e76fef2eb8ea6f6fc967886f71d88fe
Joanna Chen <joanna@dhcp-v106-079.mobile.uci.edu>
2023-05-17 15:28:14 -0700

    wrote retrieval.py for boolean retrieval

--- generate_index.py
+++ generate_index.py
@@ -9,13 +9,13 @@ from nltk.tokenize import word_tokenize
 import os, json, pickle
 from collections import defaultdict
 from invert_index import InvertedIndex
-#import requests
+# import requests
 import time
 
 word_freq = defaultdict(int)
-ID_dict = defaultdict(str) #{doc_id: url}
+ID_dict = defaultdict(str)  # {doc_id: url}
 ID_count = 1
-TEST_SIZE = 10
+TEST_SIZE = 99999
 
 index_dict = InvertedIndex()
 
@@ -31,7 +31,7 @@ def createID(url):
 
 
 def read_files():
-    #path_to_json = 'DEV/www_ics_uci_edu/'
+    # path_to_json = 'DEV/www_ics_uci_edu/'
     path_to_json = 'DEV/'
     # Get all the directories within the path
     dirs = [path for path in os.listdir(path_to_json)]
@@ -43,11 +43,12 @@ def read_files():
         print(path)
         # Process files from each directory
         dir_files = [pos_json for pos_json in os.listdir(path) if pos_json.endswith('.json')]
-        process_files(path,dir_files)
-    
-def process_files(json_path,files):
+        process_files(path, dir_files)
+
+
+def process_files(json_path, files):
     counter = 1
-    print(f"STARTING: {json_path}, TEST SIZE: {str(TEST_SIZE)}" )
+    print(f"STARTING: {json_path}, TEST SIZE: {str(TEST_SIZE)}")
     for file_name in files:
         # # politeness 0.3s:
         # time.sleep(0.3)
@@ -93,15 +94,16 @@ def process_files(json_path,files):
             #     # error in sending the request
             #     print("Request ERROR")
             #     continue
-            
+
             # check if url is duplicated
             doc_ID = createID(url)
-            #print(f'doc_ID: {doc_ID}')
+            # print(f'doc_ID: {doc_ID}')
 
             soup = BeautifulSoup(content, "html.parser")
             text = soup.get_text()
 
-            tokens = tokenize(text,doc_ID)
+            tokens = tokenize(text, doc_ID)
+
 
 def tokenize(text, doc_ID):
     # nltk to tokenize the text provided
@@ -118,49 +120,51 @@ def tokenize(text, doc_ID):
                 index_dict.index[word][doc_ID] = 1
     return list(set(tokens))
 
+
 # start the program
-read_files()
+if __name__ == '__main__':
+    read_files()
 
-sorted_index = sorted(index_dict.index.items())
+    sorted_index = sorted(index_dict.index.items())
 
-count = 0
+    count = 0
 
-f2 = open('main_index.txt', "w")
+    f2 = open('main_index.txt', "w")
 
-with open('index.txt', 'w') as f:
-    f2.write(str(len(sorted_index)//100) + '\n')
-    for i in sorted_index:
-        if count % 100 == 0:
-            f2.write(f"{i[0]} {count}\n")
-        f.write(str(i)+'\n')
-        count += 1
-f2.close()
+    with open('index.txt', 'w') as f:
+        # f2.write(str(len(sorted_index)//100) + '\n')
+        for i in sorted_index:
+            if count % 1000 == 0:
+                f2.write(f"{i[0]} {os.stat('index.txt').st_size}\n")
+            f.write(f'{i[0]}---{i[1]}\n')
+            f.flush()
+            count += 1
+    f2.close()
 
+    with open('urls.json', 'w') as f3:
+        json.dump(ID_dict, f3)
 
+    # get size of the file in KB
+    file_size = os.path.getsize('index.txt') / 1024
 
+    # generate report
+    with open('report.txt', 'w+') as file:
+        file.write("Team Members:\n")
+        file.write("Joanna Chen, 66238804\n")
+        file.write("Yui Guo, 75458764\n")
+        file.write("Weiyu Hao, 59955246\n")
+        file.write("Ruiyang Wang, 52294785\n")
+        file.write("\n\n\n\n")
+        file.write("Number of Documents: " + str(ID_count))
+        file.write("\n\n")
+        file.write("Number of Unique Tokens: " + str(len(index_dict.index)))
+        file.write("\n\n")
+        file.write("File size of index in disk is " + str(round(file_size, 2)) + "KB")
+        file.write("\n\n")
+        file.write("DocID with URLs:\n")
+        for k, v in ID_dict.items():
+            file.write(f'DocID {k} ----- {v}\n')
 
-# get size of the file in KB
-file_size = os.path.getsize('index.pkl') / 1024
-
-# generate report
-with open('report.txt', 'w+') as file:
-    file.write("Team Members:\n")
-    file.write("Joanna Chen, 66238804\n")
-    file.write("Yui Guo, 75458764\n")
-    file.write("Weiyu Hao, 59955246\n")
-    file.write("Ruiyang Wang, 52294785\n")
-    file.write("\n\n\n\n")
-    file.write("Number of Documents: " + str(ID_count))
-    file.write("\n\n")
-    file.write("Number of Unique Tokens: " + str(len(index_dict.index)))
-    file.write("\n\n")
-    file.write("File size of index in disk is " + str(round(file_size,2)) + "KB")
-    file.write("\n\n")
-    file.write("DocID with URLs:\n")
-    for k,v in ID_dict.items():
-        file.write(f'DocID {k} ----- {v}\n')
-
-# for k,v in index_dict.index.items():
-#     print(f"###########{k}: -------------{v}")
-#     print(f'url {ID_dict[list(v.keys())[0]]} for key {v}')
-
+    # for k,v in index_dict.index.items():
+    #     print(f"###########{k}: -------------{v}")
+    #     print(f'url {ID_dict[list(v.keys())[0]]} for key {v}')
--- retrieval.py
+++ retrieval.py
@@ -1,29 +1,81 @@
+import json
+import time
 
-query = input("Please enter your query: ")
+query = ''
+print("   ########## ##########")
+print("WELCOME TO OUR SEARCH ENGINE")
+while True:
+    query = input("> ").lower()
+    start_time = time.time()
+    if query == '!quit':
+        break
+    # sort words for query
+    words = sorted(query.split())
 
-words = query.split()
+    byte_pos = dict()
+    with open('main_index.txt', 'r+') as f:
+        text = f.readline()
+        pointer = 0
+        last = text.split()[1]
+        while text != '' and (pointer < len(words)):
+            token = text.split()[0]
 
-with open('main_index.txt', 'r+') as f:
-    line_num = 0
-    found = 0
-    for word in words:
-        left = 1
-        f.seek(0)
-        right = int(f.readline()) + 1
-        while left <= right:
-            mid = (left + right) // 2
-            f.seek(mid)
+            # Comparison between tokens, save the byte position
+            if token == words[pointer]:
+                byte_pos[words[pointer]] = text.split()[1]
+                pointer += 1
+                continue
+            elif token > words[pointer]:
+                byte_pos[words[pointer]] = last
+                pointer += 1
+                continue
+            last = text.split()[1]
             text = f.readline()
-            print(f'ori: {text}')
-            print(text.split()[0])
-            if f.readline().split()[0] == word:
-                found = 1
-                line_num = mid
-            elif f.readline().split()[0] < word:
-                left = mid + 1
+        # If not found, all append last position
+        while pointer < len(words):
+            byte_pos[words[pointer]] = last
+            pointer += 1
+    # List of docID sets
+    ID_sets = set()
+    # print(byte_pos)
+    f2 = open('index.txt')
+    for k, v in byte_pos.items():
+        # print(f"inside {k},{v}")
+        f2.seek(int(v))
+        token_tuple = tuple(f2.readline().rstrip('\n').split('---'))
+        # print(token_tuple)
+        while len(token_tuple) != 0 and token_tuple[0] < k:
+            # print(token_tuple[0],k)
+            # print("1")
+            token_tuple = tuple(f2.readline().rstrip('\n').split('---'))
+        if token_tuple[0] == k:
+            corr_dict = eval(token_tuple[1]).keys()
+            # Intersecting while adding to the set
+            # print(corr_dict)
+            if len(ID_sets) == 0:
+                ID_sets = set(corr_dict)
             else:
-                right = mid - 1
-        if not found:
-            line_num = left
-    print(line_num)
+                ID_sets = ID_sets.intersection(set(corr_dict))
+        else:
+            ID_sets = set()
+            break
+        # print(token_dt)
+    f2.close()
+    # print(ID_sets)
 
+    if len(ID_sets) != 0:
+        print(f"{len(ID_sets)} Results in ", end="")
+        print("%s ms:" % round((time.time() - start_time) * 1000, 2))
+        # generate URLs
+        f = open('urls.json')
+        ID_dict = json.load(f)
+        rank = 1
+        for i in ID_sets:
+            print(f'{rank}: {ID_dict[str(i)]}')
+            rank += 1
+            if rank > 10:
+                break
+        f.close()
+    else:
+        print('No Result')
+        print("--- %s ms ---" % round((time.time() - start_time) * 1000, 2))
