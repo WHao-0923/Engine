bd49ea0cd34a92d0956418e10303f1db2b957d01
Weiyu Hao <78854240+WHao-0923@users.noreply.github.com>
2023-06-02 10:17:47 -0700

    Update server.py

--- server.py
+++ server.py
@@ -10,8 +10,30 @@ def home():
 @app.route('/search', methods=['GET'])
 def search():
     query = request.args.get('query')
-    results,summary,speed = retrieval.search(query)
-    return render_template('ZotSearchResult.html', results=results, summary=summary, speed=speed)
+    page = int(request.args.get('page', 1))  # Get current page number, default to 1
+    results_per_page = 10  # Number of results per page
+
+    # Call your retrieval.search() function to get the search results, summary, and speed
+    results, summary, speed = retrieval.search(query)
+
+    # Determine the start and end indices of the results to display for the current page
+    start_index = (page - 1) * results_per_page
+    end_index = start_index + results_per_page
+
+    # Subset the results based on the current page
+    results_subset = results[start_index:end_index]
+
+    # Determine previous and next page URLs
+    previous_page = None
+    if page > 1:
+        previous_page = request.base_url + f"?query={query}&page={page - 1}"
+
+    next_page = None
+    if end_index < len(results):
+        next_page = request.base_url + f"?query={query}&page={page + 1}"
+
+    return render_template('ZotSearchResult.html', results=results_subset, summary=summary, speed=speed,
+                           previous_page=previous_page, next_page=next_page)
 
 if __name__ == '__main__':
     app.run(debug=True)
